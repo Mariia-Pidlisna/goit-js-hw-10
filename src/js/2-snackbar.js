6d8667290f9edfcc2c8076b687fa5df9dfaa84ea
mariia.pidlisna <mariia.pidlisna@gmail.com>
2025-03-30 09:38:20 +0300

    add all files

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,85 @@
-console.log("snackbar");
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault(); // Щоб форма не перезавантажувала сторінку
+
+    const delay = Number(event.target.elements.delay.value); // Отримуємо затримку
+    const shouldResolve = event.target.elements.state.value === "fulfilled"; // Отримуємо стан з radio-кнопок
+
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+    .then((delay) => {
+        iziToast.success({
+            title: "Ok",
+            message: `Fulfilled promise in ${delay}ms`,
+            position: "topRight",
+            class: "icon-svg-success",
+            iconUrl: "../img/bi_check2-circle.svg",
+            backgroundColor: "#59a10d",
+            titleColor: "#fff",
+            messageColor: "#fff",
+        });
+    })
+    .catch((delay) => {
+        iziToast.error({
+            title: "Error",
+            message: `Rejected promise in ${delay}ms`,
+            position: "topRight",
+            class: "icon-svg-error",
+            backgroundColor: "#ef4040",
+            titleColor: "#fff",
+            messageColor: "#fff",
+            iconUrl: "../img/bi_x-octagon.svg",
+            
+        });
+    });
+}
+
+
+
+
+
+
+// Напиши скрипт, який після сабміту форми створює проміс. 
+// В середині колбека цього промісу через вказану користувачем кількість 
+// мілісекунд проміс має виконуватися (при fulfilled) або 
+// відхилятися (при rejected), залежно від обраної опції в радіокнопках. 
+// Значенням промісу, яке передається як аргумент у методи resolve/reject, 
+// має бути значення затримки в мілісекундах.
+
+// Створений проміс треба опрацювати у відповідних для вдалого/невдалого 
+// виконання методах.
+
+// Якщо проміс виконується вдало, виводь у консоль наступний рядок, де
+//  delay — це значення затримки виклику промісу в мілісекундах.
+
+//  `✅ Fulfilled promise in ${delay}ms`
+
+
+// Якщо проміс буде відхилено, то виводь у консоль наступний рядок, 
+// де delay — це значення затримки промісу в мілісекундах.
+
+// `❌ Rejected promise in ${delay}ms`
+
+
+
+// Бібліотека повідомлень
+
+
+
+// Для відображення повідомлень, замість console.log(), використовуй 
+// бібліотеку iziToast. Для того щоб підключити CSS код бібліотеки в 
+// проєкт, необхідно додати ще один імпорт, крім того, що описаний у документації.
+
